c87b1ae25b0868dd64af00fa6a7e27bf5bd89d79
evanandrewson <evanandrewson@gmail.com>
2019-09-29 12:51:02 -0700

    added request handlers

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log('request for ' + pathname + ' received');
 
-    route(pathname);
+    route(handle, pathname);
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('hello world')
