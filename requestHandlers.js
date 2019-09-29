e8d72776cf4ccb40a4b7aab438873fdba905d484
evanandrewson <evanandrewson@gmail.com>
2019-09-29 14:37:02 -0700

    added image upload functionality

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,20 @@
 const querystring = require('querystring');
+const fs = require('fs');
+const Formidible = require('formidable');
 
 function start(res) {
   console.log(`Request handler 'start' was called.`);
 
-  var body = '<html>'+
+  const body = '<html>'+
   '<head>'+
-  '<meta http-equiv="Content-Type" content="text/html; '+
-  'charset=UTF-8" />'+
+  '<meta http-equiv="Content-Type" '+
+  'content="text/html; charset=UTF-8" />'+
   '</head>'+
   '<body>'+
-  '<form action="/upload" method="post">'+
-  '<textarea name="text" rows="20" cols="60"></textarea>'+
-  '<input type="submit" value="Submit text" />'+
+  '<form action="/upload" enctype="multipart/form-data" '+
+  'method="post">'+
+  '<input type="file" name="upload" multiple="multiple">'+
+  '<input type="submit" value="Upload file" />'+
   '</form>'+
   '</body>'+
   '</html>';
@@ -21,12 +24,32 @@ function start(res) {
   res.end();
 }
 
-function upload(res, postData) {
+function upload(res, req) {
   console.log(`Request handler 'upload' was called.`);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('You\'ve sent the text' + querystring.parse(postData).text);
+
+  const form = new Formidible.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, (error, fields, files) => {
+    console.log('parsing done');
+    fs.rename(files.upload.path, '/tmp/test.png', error => {
+      if(error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+  })
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('received image: <br/>');
+  res.write(`<img src='/show' />`)
   res.end();
+}
+
+function show(res) {
+  console.log(`Request handler 'show' was called.`);
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -3,25 +3,13 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest(req, res) {
-    var postData = '';
     var pathname = url.parse(req.url).pathname;
     console.log('request for ' + pathname + ' received');
-
-    req.setEncoding('utf8');
-
-    req.addListener('data', postDataChunk => {
-      postData += postDataChunk;
-      console.log(`Receieved POST data chunk '${postDataChunk}'.`)
-    });
-
-    req.addListener('end', () => {
-      route(handle, pathname, res, postData);
-    });
+    route(handle, pathname, res, req);
   }
 
   http.createServer(onRequest).listen(8888);
   console.log('server has started');
 }
-
 
 exports.start = start;
