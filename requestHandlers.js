37ca32e9144a4a416f645896353bcaa9ea333a6a
evanandrewson <evanandrewson@gmail.com>
2019-09-29 13:26:17 -0700

    changed request handlers to be non blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,23 @@
-function start() {
+const exec = require('child_process').exec;
+
+function start(res) {
   console.log(`Request handler 'start' was called.`);
+  const content = 'empty';
 
-  function sleep(milliSeconds) {
-    const startTime = new Date().getTime();
-    while(new Date().getTime() < startTime + milliSeconds);
-  }
+  exec('ls -lah', (error, stdout, stderr) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(stdout);
+    res.end();
+  });
 
-  sleep(10000);
-  return 'Hello Start';
+  return content;
 }
 
-function upload() {
+function upload(res) {
   console.log(`Request handler 'upload' was called.`);
-  return 'Hello Upload';
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello Upload');
+  res.end();
 }
 
 exports.start = start;
