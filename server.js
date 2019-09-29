9d6cf263a0e828b2055138634898c462dda54710
evanandrewson <evanandrewson@gmail.com>
2019-09-29 12:40:30 -0700

    added simple router

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const http = require('http');
+const url = require('url');
 
-function start() {
+function start(route) {
   function onRequest(req, res) {
-    console.log('request received');
+    var pathname = url.parse(req.url).pathname;
+    console.log('request for ' + pathname + ' received');
+
+    route(pathname);
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('hello world')
     res.end();
   }
-  
+
   http.createServer(onRequest).listen(8888);
   console.log('server has started');
 }
